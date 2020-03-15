959c1bbff07c7f9c36b659091d9f72c7c5858c1a
marctanyh <marctanyh@gmail.com>
2020-03-16 06:52:13 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,22 +2,31 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push("cardOne");
-
-console.log("User flipped queen");
-
-let cardTwo = cards[2];
-
-cardsInPlay.push("cardTwo")
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    result = alert("You found a match!");
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
 }
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
-    result = alert("Sorry, try again.");
+function flipCard(cardId) {
+
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        result = alert("You found a match!");
+    }
+
+    if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+        result = alert("Sorry, try again.");
+    }
+
+    console.log("User flipped" + cards[cardId]);
+
+    cardsInPlay.push(cards[cardId]);
 }
+
+flipCard(0);
+
+flipCard(2);
+
+checkForMatch(0);
