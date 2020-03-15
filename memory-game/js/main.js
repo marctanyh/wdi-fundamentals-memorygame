6afe06e0b4e16591562fd387d8faded98ff0b0df
marctanyh <marctanyh@gmail.com>
2020-03-16 07:23:35 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+},
+];
 
 const cardsInPlay = [];
 
@@ -20,9 +41,13 @@ function flipCard(cardId) {
         result = alert("Sorry, try again.");
     }
 
-    console.log("User flipped" + cards[cardId]);
+    console.log("User flipped" + " " +cards[cardId].rank);
 
-    cardsInPlay.push(cards[cardId]);
+    console.log(cards[cardId].cardImage);
+
+    console.log(cards[cardId].suit);
+
+    cardsInPlay.push(cards[cardId].rank);
 }
 
 flipCard(0);
